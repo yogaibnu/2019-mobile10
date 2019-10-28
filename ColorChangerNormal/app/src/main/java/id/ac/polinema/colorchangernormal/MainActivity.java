8e121bffebdeeca8c1d4bf39feed19728e535a93
yogaibnu <yogaibnudinataa@gmail.com>
2019-10-28 13:57:28 +0700

    ViewModel Sederhana Percobaan 1

--- ColorChangerNormal/app/src/main/java/id/ac/polinema/colorchangernormal/MainActivity.java
+++ ColorChangerNormal/app/src/main/java/id/ac/polinema/colorchangernormal/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import java.util.Random;
@@ -24,6 +25,13 @@ public class MainActivity extends AppCompatActivity {
 		rootView.setBackgroundColor(generateRandomColor());
 
 		// Tambahkan event klik pada tombol
+		btnChangeColor.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				int color = generateRandomColor();
+				rootView.setBackgroundColor(color);
+			}
+		});
 	}
 
 	private int generateRandomColor(){
